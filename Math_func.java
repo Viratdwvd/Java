5095ed9149427cf26a4db116ebb6dd6e4e92a232
Virat Dwivedi <147337909+Viratdwvd@users.noreply.github.com>
2023-10-21 19:50:31 +0530

    Create Math_func.java
    Math functions in java such as sum,abs,pow etc.

---
+++ Math_func.java
@@ -0,0 +1,14 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    double a=30d;
+	    double b=2;
+	    double d=145.256;
+	    double c=1.0;
+	    float x=4567.9876f;
+		System.out.println(Math.pow(a,b));
+		System.out.println(Math.log(d));
+		System.out.println(Math.round(x));
+		System.out.println(Math.asin(c));
+	}
+}
